afd52f3b005076c80c79f0dffdb0a6289c40e4ae
patelajay745 <patel.ajay745@gmail.com>
2024-01-10 14:20:21 -0500

    updated stop container file

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,2 +1,2 @@
-docker pull patelajay745/sample-python
-docker run --name sample -d -p 80:5000 patelajay745/sample-python
+docker pull patelajay745/sample-python:latest
+docker run --name sample -d -p 5000:5000 patelajay745/sample-python:latest
