f0252d263b5732be45c0fb512c529080e7446957
patelajay745 <patel.ajay745@gmail.com>
2024-01-10 13:41:44 -0500

    updated stop container file

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-CONTAINER_NAME="sample"
-
-# Check if the container exists and is running
-if docker inspect -f '{{.State.Running}}' "$CONTAINER_NAME" &>/dev/null; then
-    # Container exists and is running, so stop and remove it
-    docker stop "$CONTAINER_NAME"
-    docker rm "$CONTAINER_NAME"
-else
-    echo "Container $CONTAINER_NAME does not exist or is not running."
+if docker inspect sample &> /dev/null; then
+    docker stop sample
+    docker rm sample
 fi
